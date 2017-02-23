26a617d8219c41e1729db7ada8c5367a2ee6868d
MaksimPegov <maksim.peg@gmail.com>
2017-02-23 14:52:25 +0200

    добавили бэйджики

--- src/main.js
+++ src/main.js
@@ -9,15 +9,16 @@ $(function() {
 
 
   function success(respons){
-    debugger
     var badge;
-    var parent = $("#badges");
+    var parent = $("#bages");
     var finished = respons.courses.completed;
     for(var i = 0; i < finished.length; i++){
-      badge =  $("<div class='course'>");
-      badge.append($("<h3>").text(finished[i].title));
-      badge.append($("<img src='" + finished[i].badge + "'>"));
-      badge.append($("<a href='" + finished[i].url + "' class ='btn btn-primary' target='_blank'>").text("See Course"))
+      //badge =  $("<div class='course'>");
+      // badge.append($("<h3>").text(finished[i].title));
+      // badge.append($("<img src='" + finished[i].badge + "'>"));
+      // badge.append($("<a href='" + finished[i].url + "' class ='btn btn-primary' target='_blank'>").text("See Course"))
+      badge =  $("<div class='badge'>");
+      badge.append($("<a href='" + finished[i].url + "' target='_blank'>").append($("<img src='" + finished[i].badge + "'>")));
       parent.append(badge);
       
     }
